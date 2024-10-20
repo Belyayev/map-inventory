0022fa2ff4bb20f09e78fbea6ac854cc81fe2895
Mike Belyayev <4xgood@gmail.com>
2024-10-20 15:48:30 -0600

    updated eslint err

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import dynamic from "next/dynamic";
 import { useAuth } from "@clerk/nextjs";
-// import dbConnect from "./utils/dbConnect";
 import Header from "./components/Header";
-// import Modal from "react-modal"; // import Modal
+import dbConnect from "./utils/dbConnect";
+import { LatLngTuple } from "leaflet"; // Import the LatLngTuple type
 
 // Dynamically import the MapComponent
 const MapComponent = dynamic(() => import("./components/MapComponent"), {
@@ -14,34 +14,29 @@ const MapComponent = dynamic(() => import("./components/MapComponent"), {
 
 export default function Home() {
   const { isLoaded } = useAuth();
-  const [coordinates] = useState([
+  const [coordinates] = useState<{ position: LatLngTuple; info: string }[]>([
     { position: [50.92275, -114.09211], info: "Object 1" },
     { position: [50.92275, -114.09211], info: "Object 2" },
     { position: [50.92429, -114.08978], info: "Object 3" },
   ]);
 
-  // useEffect(() => {
-  //   const connectDb = async () => {
-  //     try {
-  //       await dbConnect();
-  //       console.log("Database connected");
-  //     } catch (error) {
-  //       console.error("Database connection error:", error);
-  //     }
-  //   };
-  //   connectDb();
-
-  //   // Set the app element to avoid the warning on client-side only
-  //   if (typeof document !== "undefined") {
-  //     Modal.setAppElement(document.body);
-  //   }
-  // }, []);
+  useEffect(() => {
+    const connectDb = async () => {
+      try {
+        await dbConnect();
+        console.log("Database connected");
+      } catch (error) {
+        console.error("Database connection error:", error);
+      }
+    };
+    connectDb();
+  }, []);
 
   if (!isLoaded) {
     return <div>Loading...</div>;
   }
 
-  const center = [50.92215, -114.09333]; // Shell gas station in Calgary, AB
+  const center: LatLngTuple = [50.92215, -114.09333]; // Shell gas station in Calgary, AB
 
   return (
     <div className="min-h-screen flex flex-col">
